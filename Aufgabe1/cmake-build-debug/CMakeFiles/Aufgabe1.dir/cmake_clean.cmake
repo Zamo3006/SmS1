037ebc44f6ca75c8957bc6a7ef964b048166cd99
Budig <2>
2018-03-26 09:20:10 +0200

    moved

---
+++ Aufgabe1/cmake-build-debug/CMakeFiles/Aufgabe1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Aufgabe1.dir/main.c.o"
+  "CMakeFiles/Aufgabe1.dir/one.c.o"
+  "Aufgabe1.pdb"
+  "Aufgabe1.exe"
+  "libAufgabe1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Aufgabe1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
